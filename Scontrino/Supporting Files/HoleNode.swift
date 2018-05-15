cd72909a1c8d60408be6a1aa0d5f31bd8970eb6f
Heyosit <alessio.perrotti-apple@unina.it>
2018-05-15 16:49:02 +0200

    HoleNode file created

---
+++ Scontrino/Supporting Files/HoleNode.swift
@@ -0,0 +1,9 @@
+//
+//  HoleNode.swift
+//  Scontrino
+//
+//  Created by Alessio Perrotti on 15/05/2018.
+//  Copyright © 2018 Eduardo Yutaka Nakanishi. All rights reserved.
+//
+
+import Foundation
